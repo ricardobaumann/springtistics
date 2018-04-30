53d4ad5c4b4be4b274a53df6e82e54a7bff4ad88
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-04-30 09:47:34 +0200

    adding missing spec

--- src/test/java/com/github/ricardobaumann/springtistics/TransactionsControllerTest.java
+++ src/test/java/com/github/ricardobaumann/springtistics/TransactionsControllerTest.java
@@ -10,11 +10,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -66,5 +69,24 @@ public class TransactionsControllerTest {
                 .andExpect(status().isBadRequest());
 
         verify(transactionService, never()).createTransaction(any());
+    }
+
+    @Test
+    public void shouldMapTransactionSummary() throws Exception {
+        //Given
+        TransactionsSummary summary = new TransactionsSummary(100.0, 10.0, 20.0, 5.0, 10);
+        when(transactionService.getCurrentSummary()).thenReturn(summary);
+
+        //When //Then
+        mockMvc.perform(get("/transactions")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.sum", is(summary.getSum())))
+                .andExpect(jsonPath("$.avg", is(summary.getAvg())))
+                .andExpect(jsonPath("$.max", is(summary.getMax())))
+                .andExpect(jsonPath("$.min", is(summary.getMin())))
+                .andExpect(jsonPath("$.count", is(summary.getCount())))
+
+        ;
     }
 }
